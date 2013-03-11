5fe2c9e1dd6bf53762ad058ce74db9cd59ef5831
ted80-PC <meatkroket@hotmail.com>
2013-03-11 19:26:33 +0100

    added basic send message

--- src/client/Client.java
+++ src/client/Client.java
@@ -13,6 +13,7 @@ public class Client extends JFrame
     private JButton sendButton;
     
     //private Sender sender = new Sender();
+    private SendMessage message = new SendMessage();
  
     public Client()
     {
@@ -44,6 +45,7 @@ public class Client extends JFrame
     	{
             public void actionPerformed(ActionEvent e)  
             {
+            	message.SendClientMessage("client-name", "client-pass", inputField.getText(), inputIP.getText(), Integer.parseInt(inputPort.getText()));
             	//sender.Send(inputField.getText(), inputIP.getText(), Integer.parseInt(inputPort.getText()));
             	System.out.println(inputField.getText() +" "+ inputIP.getText() +" "+ Integer.parseInt(inputPort.getText()));
             	inputField.setText("");
--- src/client/SendMessage.java
+++ src/client/SendMessage.java
@@ -1,4 +1,18 @@
 
-public class SendMessage {
-
+public class SendMessage extends Send
+{
+	public SendMessage()
+	{
+		
+	}
+	
+	public void SendClientMessage(String name, String pass, String message, String server, int port)
+	{
+		//send to server
+		String info = "";
+		info = name + "#" + "pass" + "#" + message;
+		sendInfoToServer(info, server, port);
+		
+		//display on client
+	}
 }
