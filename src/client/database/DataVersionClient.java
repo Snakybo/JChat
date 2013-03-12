3d2ef61fbfc230a21bf532ee8578910fe1925171
ted80-Laptop <meatkroket@hotmail.com>
2013-03-12 10:10:14 +0100

    Added version check (not implented yet!)

---
+++ src/client/database/DataVersionClient.java
@@ -0,0 +1,15 @@
+package database;
+
+public class DataVersionClient 
+{
+	public DataVersionClient()
+	{
+		
+	}
+	
+	public String GetClientVersion()
+	{
+		//returns 2 things: newest version and minimum required version to run
+		return "";
+	}
+}
---
+++ src/server/database/DataVersionServer.java
@@ -0,0 +1,15 @@
+package database;
+
+public class DataVersionServer
+{
+	public DataVersionServer()
+	{
+		
+	}
+	
+	public String GetServerVersion()
+	{
+		//returns 2 things: newest version and minimum required version to run
+		return "";
+	}
+}
