c989fbe2d8a73d6bbf3e97ad774d248235825e90
ted80-Laptop <meatkroket@hotmail.com>
2013-03-18 15:39:52 +0100

    server data sender

--- src/server/server/database/DataServer.java
+++ src/server/server/database/DataServer.java
@@ -1,14 +1,34 @@
 package server.database;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DataServer 
 {
-	public DataServer()
+	public String sendData(String data)
 	{
-		
-	}
-	
-	public boolean SendData(String info)
-	{
-		return false;
+		URL url = null;
+		BufferedReader in = null;
+		String returnINFO = "";
+
+		try 
+		{ 
+			url = new URL(data); 
+			in = new BufferedReader(new InputStreamReader(url.openStream())); 
+			String inputLine; 
+			while ((inputLine = in.readLine()) != null)  
+			{
+				returnINFO += inputLine;
+			}  
+		} 
+		catch (MalformedURLException e) 
+		{ System.out.println("Cannot connect to server"); }
+		catch (IOException e) 
+		{ System.out.println("Cannot read from server"); }
+
+		return returnINFO;
 	}
 }
