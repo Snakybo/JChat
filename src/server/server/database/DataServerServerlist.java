7c8e09567985730fafdda21882b6b57bb77f7852
ted80-PC <meatkroket@hotmail.com>
2013-03-18 21:02:56 +0100

    serverlist updater

--- src/server/server/database/DataServerServerlist.java
+++ src/server/server/database/DataServerServerlist.java
@@ -4,14 +4,25 @@ import server.JServer;
 
 public class DataServerServerlist extends DataServer
 {
+	public String[][] translatechars = {{" ","%20"},{"&","[a]"},{"#","[n]"}};
+	
 	public void UpdateServer()
 	{
-		
-		
-		boolean connection = sendData("http://" + JServer.database + "/?type=SERVER_UPDATE&name=" + JServer.serverName + "&data=" + JServer.serverIP);
+		String name = Translate(JServer.serverName);
+		boolean connection = sendData("http://" + JServer.database + "/?type=SERVER_UPDATE&name=" + name + "&data=" + JServer.serverIP + ":" + JServer.serverPort);
 		if(connection == false)
 		{
 			//database is down
 		}
+	}
+	
+	public String Translate(String text)
+	{
+		String newtext = text;
+		for(int i = 0; i < translatechars.length; i++)
+		{
+			newtext = newtext.replaceAll(translatechars[i][0], translatechars[i][1]);
+		}
+		return newtext;
 	}
 }
